363d1ce92250a1d99c00948d2974cae52be596b4
Gustavo Lopez <tavoivan@gmail.com>
2017-08-25 17:04:51 +0000

    First commit

---
+++ src/com/formmaker/Main.java
@@ -0,0 +1,19 @@
+package com.formmaker;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class Main {
+
+	private Properties properties;
+	private Connection conn;
+	private String database;
+	private ArrayList<String> tables;
+	private String outPath;
+
+	public static void main(String args[])
+	{
+		
+	}
+}
