0c10456867b7e0cb1bf1ed8101f399d57a2e29b3
Gustavo Ivan <tavoivan@gmail.com>
2017-09-09 05:50:44 +0000

    load config

--- src/com/formmaker/Main.java
+++ src/com/formmaker/Main.java
@@ -14,6 +14,38 @@ public class Main {
 
 	public static void main(String args[])
 	{
-		
+		boolean lookForFile = false;
+		properties = new Properties();
+
+		int cont = 0;
+
+		for (String arg : args)
+		{
+			// File section
+			if (lookForFile)
+			{
+				try (FileInputStream in = new FileInputStream(arg))
+				{
+					properties.load(in);
+				}
+				catch (FileNotFoundException e)
+				{
+					System.out.println("The file: \"" + arg + "\" does not exist");
+					return;
+				}
+			}
+
+			if (arg.equals("-f"))
+			{
+				lookForFile = true;
+
+				continue;
+			}
+
+			// Normal section
+			
+
+			cont++;
+		}
 	}
 }
